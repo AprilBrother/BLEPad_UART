3638e20fa82956cd45edaa6188acf9fad648fe24
volca <volca@tom.com>
2016-04-05 22:30:18 +0800

    method

--- BLEPad_UART.h
+++ BLEPad_UART.h
@@ -1,13 +1,12 @@
-#ifndef  _AB_BLE_H
-#define _AB_BLE_H
+#ifndef  _BLEPAD_UART_H
+#define _BLEPAD_UART_H
 
 #include <Arduino.h>
 
-class AB_BLE : public Print {
+class BLEPad_UART : public Stream{
 
     public:
-        AB_BLE(Stream *s = &Serial1);
-        void writeBytes(unsigned char *data, unsigned char len);
+        BLEPad_UART(Stream *s = &Serial1);
         void println(const char data[]);
 
         int available();
@@ -16,10 +15,6 @@ class AB_BLE : public Print {
         // Class Print virtual function Interface
         virtual size_t write(uint8_t c);
         virtual size_t println(int, int = DEC);
-
-
-        // pull in write(str) and write(buf, size) from Print
-        using Print::write;
 
     private:
         Stream    *stream;     // -> BLE module, e.g. SoftwareSerial or Serial1
